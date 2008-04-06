8bbe6cd2c8acf1c21133375b7ae5e6eede08d349
Benedict Eastaugh <ionfish@gmail.com>
2008-04-06 22:36:41 +0100

    New Rake file to generate static pages, require the Rubypants gem in the environment config

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,9 @@
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  # Require the latest Rubypants gem
+  config.gem "rubypants"
+  
   config.action_controller.session = {
     :session_key => "_papertrail_session",
     :secret => "7c911fb269c1733f19ec5ac4457dd7e2aa62c9135a5e5d13476be5816960958789479d39b3c349cd646f87f3e379322254467dda1ac4cca6f2e2b4f20930a686"
---
+++ lib/tasks/static.rake
@@ -0,0 +1,12 @@
+namespace :static do
+  
+  desc "Create a 404 error page from the default layout"
+  task :err404 do
+    
+  end
+  
+  desc "Create a 500 error page from the default layout"
+  task :err500 do
+  end
+  
+end
