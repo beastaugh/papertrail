8bc8f069c104a53731c64fc5e7d2708862cfb99f
Benedict Eastaugh <benedict@eastaugh.net>
2008-06-29 16:03:39 +0100

    Add some documentation to the book editing JS

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -1,4 +1,13 @@
+/**
+ * Ajax editing for books: dynamically loads edit forms into the current
+ * context and replaces them with updated content on save.
+ *
+ * TODO: refactor the code so it works for authors as well as books.
+ */
 $(document).ready(function() {
+  /**
+   * When an edit button is clicked, load the edit form for that book.
+   */
   $('#content').delegate('click', {
     '.edit': function(event) {
       var link = $(event.target);
@@ -11,6 +20,12 @@ $(document).ready(function() {
     }
   });
   
+  /**
+   * When an edit form is submitted, replace it with the book details.
+   *
+   * TODO: add error handling so that a failed request doesn't replace the form
+   * with nothing at all.
+   */
   $('#content').delegate('submit', {
     'form.edit_book': function(event) {
       var form = $(event.target);
