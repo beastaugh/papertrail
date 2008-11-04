1ff425c35d8fb1d379d85dada6b1da00cfcb723f
Benedict Eastaugh <benedict@eastaugh.net>
2008-11-04 00:35:31 +0000

    Clean up Authorship model a bit

--- app/models/authorship.rb
+++ app/models/authorship.rb
@@ -1,14 +1,6 @@
 class Authorship < ActiveRecord::Base
-  attr_accessible :author_id, :book_id, :weight
   belongs_to :author
   belongs_to :book
-  before_validation :generate_weight
-  validates_presence_of :author_id, :book_id, :weight
+  validates_presence_of :author_id, :book_id
   validates_uniqueness_of :author_id, :scope => :book_id
-  
-  protected
-  
-  def generate_weight
-    self.weight = 0
-  end
 end
