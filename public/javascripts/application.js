c811d54ca7a3f47c42d273e1295753ada332f2cd
Benedict Eastaugh <benedict@eastaugh.net>
2008-11-18 15:30:16 +0000

    Hide notices faster.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,9 +5,9 @@ jQuery(document).ready(function() {
   
   setTimeout(function() {
     jQuery('.notice').each(function() {
-      jQuery(this).animate({marginBottom: 0, height: 0, opacity: 0}, 500, 'linear', function() {
+      jQuery(this).animate({marginBottom: 0, height: 0, opacity: 0}, 250, 'linear', function() {
         jQuery(this).remove();
       });
     });
-  }, 5*1000);
+  }, 3*1000);
 });
