672e343b5ce0d6e4ed713baec8dc7cbf9993cfb7
Benedict Eastaugh <benedict@eastaugh.net>
2008-11-18 15:25:04 +0000

    Hide notices after five seconds.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,4 +2,12 @@ jQuery(document).ready(function() {
   jQuery('.editable').each(function() {
     var editable = new Editable(this);
   });
+  
+  setTimeout(function() {
+    jQuery('.notice').each(function() {
+      jQuery(this).animate({marginBottom: 0, height: 0, opacity: 0}, 500, 'linear', function() {
+        jQuery(this).remove();
+      });
+    });
+  }, 5*1000);
 });
