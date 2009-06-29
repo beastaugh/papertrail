32259315bb1d60df6113524e440cf6b6a535701e
Benedict Eastaugh <benedict@eastaugh.net>
2009-06-29 23:58:45 +0100

    Remove now-defunct login and sessions code.

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,10 @@ ActionController::Routing::Routes.draw do |map|
   end
   
   map.controller_actions 'books', %w[all covers]
-  map.resources :sessions, :books, :authors
-  
-  map.with_options :controller => 'sessions' do |sessions|
-    sessions.login 'login', :action => 'new'
-    sessions.logout 'logout', :action => 'destroy'
-  end
+  map.resources :books, :authors
   
   map.connect "graphs", :controller => "graphs", :action => "index"
   map.connect "graphs/frequency", :controller => "graphs", :action => "frequency"
-      
+  
   map.root :controller => 'books'
 end
