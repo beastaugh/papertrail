459174aa77e8fdaf8a54ac540f65ea8bbb0b3de3
Benedict Eastaugh <benedict@eastaugh.net>
2009-08-22 20:22:22 +0100

    Make the reading frequency histogram a bit more attractive.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -9,7 +9,11 @@ jQuery(document).ready(function() {
   
   setTimeout(function() {
     jQuery('.notice').each(function() {
-      jQuery(this).animate({marginBottom: 0, height: 0, opacity: 0}, 250, 'linear', function() {
+      jQuery(this).animate({
+        marginBottom: 0,
+        height: 0,
+        opacity: 0
+      }, 250, 'linear', function() {
         jQuery(this).remove();
       });
     });
--- public/javascripts/graphs.js
+++ public/javascripts/graphs.js
@@ -35,7 +35,9 @@ var HistogramFromTable = function(wrapper) {
   this.X       = 40;
   this.Y       = 20;
   this.GUTTER  = 2;
-  this.OFFSET  = 4;
+  this.TOP     = 27;
+  this.BOTTOM  = 4;
+  this.SIDE    = 9;
   
   this.table   = jQuery(wrapper).find('table').eq(0).hide();
   var rows     = this.table.find('tr');
@@ -45,38 +47,50 @@ var HistogramFromTable = function(wrapper) {
   this.values  = jQuery.makeArray(rows.eq(1).find('td.num_books'))
                  .map(compose(parseInt, getContents));
   
-  var width  = (this.X + this.GUTTER) * this.values.length - this.GUTTER,
-      height = maximum(this.values) * this.Y + this.OFFSET;
+  var width  = (this.X + this.GUTTER) * this.values.length - this.GUTTER + this.SIDE * 2,
+      height = maximum(this.values) * this.Y + this.TOP + this.BOTTOM;
   
   this.canvas  = jQuery('<canvas width="' + width + '" height="' + height + '"></canvas>');
-  this.context = this.canvas[0].getContext('2d');  
+  this.context = this.canvas[0].getContext('2d');
+  
+  var striped = new Image();
+  striped.src = '/images/stripes.png';
+  var self    = this, pattern;
+  
   this.table.after(this.canvas);
   
   this.insertBar = function(size, order) {
-    var x = order * (this.X + this.GUTTER),
+    var x = order * (this.X + this.GUTTER) + this.SIDE,
         y = this.Y * size;
     
-    var offset = this.canvas.attr('height') - y - this.OFFSET;
+    var offset = this.canvas.attr('height') - y - this.BOTTOM;
     
-    this.context.fillStyle = '#212426';
+    this.context.fillStyle = '#272c2e';
     this.context.fillRect(x, offset, this.X, y);
     
     var label = jQuery('<span class="key">' + this.keys[order] + '</span>');
     label.css({
       width: this.X,
-      top:   this.canvas.attr('height') + 8 - this.OFFSET,
+      top:   this.canvas.attr('height') + 8 - this.BOTTOM,
       left:  x
     });
     this.table.after(label);
-        
-    this.context.fillStyle = '#2b3033';
+    
+    this.context.fillStyle = '#212426';
     for (i = 1; i < size; i++) {
-      this.context.fillRect(order * (this.X + this.GUTTER), offset + i * this.Y, this.X, 1);
-    }    
+      this.context.fillRect(order * (this.X + this.GUTTER) + this.SIDE,
+        offset + i * this.Y, this.X, 1);
+    }
   };
   
-  this.values.forEach(this.insertBar, this);
-  
-  this.context.fillStyle = '#2b3033';
-  this.context.fillRect(0, this.canvas.attr('height') + 3 - this.OFFSET, this.canvas.attr('width'), 1);
+  jQuery(striped).load(function() {
+    self.context.fillStyle = self.context.createPattern(striped, 'repeat');
+    self.context.fillRect(0, 0, width, height - self.BOTTOM);
+    
+    self.values.forEach(self.insertBar, self);
+    
+    self.context.fillStyle = '#2b3033';
+    self.context.fillRect(0, self.canvas.attr('height') + 3 - self.BOTTOM,
+      self.canvas.attr('width'), 1);
+  });
 };
