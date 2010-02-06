81a51983f17a7ca89d69e513097184130172e692
Benedict Eastaugh <benedict@eastaugh.net>
2010-02-06 20:47:06 +0000

    Clear things up a bit in the routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Papertrail::Application.routes.draw do
   match 'books/page/:page' => 'books#index'
   match 'authors/page/:page' => 'authors#index'
   match 'books/covers' => 'books#covers', :as => :books_covers
+  
   resources :books
   resources :authors
+  
   match 'graphs' => 'graphs#index'
   match 'graphs/frequency' => 'graphs#frequency'
+  
   root :to => 'books#index'
 end
