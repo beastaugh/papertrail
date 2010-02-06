3c85ac73f8ca71825fdde93672e2bf8807523955
Benedict Eastaugh <benedict@eastaugh.net>
2010-02-06 15:13:31 +0000

    New router.

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,11 @@
-ActionController::Routing::Routes.draw do |map|
-  def map.controller_actions(controller, actions)
-    actions.each do |action|
-      self.send("#{controller}_#{action}", "#{controller}/#{action}",
-        :controller => controller, :action => action)
-    end
-  end
-  
-  map.connect "books/page/:page", :controller => "books", :action => "index"
-  map.connect "authors/page/:page", :controller => "authors", :action => "index"
-  
-  map.controller_actions 'books', %w[all covers]
-  map.resources :books, :authors
-  
-  map.connect "graphs", :controller => "graphs", :action => "index"
-  map.connect "graphs/frequency", :controller => "graphs", :action => "frequency"
-  
-  map.root :controller => 'books'
+Papertrail::Application.routes.draw do
+  match 'books/page/:page' => 'books#index'
+  match 'authors/page/:page' => 'authors#index'
+  match 'books/all' => 'books#all', :as => :books_all
+  match 'books/covers' => 'books#covers', :as => :books_covers
+  resources :books
+  resources :authors
+  match 'graphs' => 'graphs#index'
+  match 'graphs/frequency' => 'graphs#frequency'
+  match '/' => 'books#index'
 end
