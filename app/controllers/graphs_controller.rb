276cfd9f44f33b0ff014e7b5b2a2c59420804508
Benedict Eastaugh <benedict@eastaugh.net>
2010-08-29 16:56:38 +0100

    Make reading frequency histogram display months with no books read.
    Fixes gh-8.

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -10,17 +10,15 @@ class GraphsController < ApplicationController
     @title = "Frequency"
     @books = Book.last_year
     
-    @last_year = @books.inject([]) do |distribution, book|
-      name = Time::RFC2822_MONTH_NAME[book.created_at.month - 1]
-      
-      if distribution.last.nil? || distribution.last[:name] != name
-        distribution << {:name => name, :num_books => 1}
-      else
-        distribution.last[:num_books] += 1
-      end
-      
-      distribution
-    end
+    first_month  = @books.first.created_at.month
+    distribution = (1..12).map {|i|
+      {:name => Time::RFC2822_MONTH_NAME[i - 1], :num_books => 0, :index => i}
+    }
+    
+    @last_year = @books.inject(distribution) {|dist, book|
+      dist[book.created_at.month - 1][:num_books] += 1
+      dist
+    }.partition {|m| m[:index] >= first_month }.flatten
   end
   
 end
