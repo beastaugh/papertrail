351dbd236b9864824ce6fbcd40123fa44907a17b
Benedict Eastaugh <benedict@eastaugh.net>
2010-10-23 00:00:16 +0100

    Don't let books wrap round from one year to the next.

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -10,15 +10,23 @@ class GraphsController < ApplicationController
     @title = "Frequency"
     @books = Book.last_year
     
-    first_month  = @books.first.created_at.month
+    now          = Time.now
+    this_month   = now.month
+    this_year    = now.year
+    
     distribution = (1..12).map {|i|
       {:name => Time::RFC2822_MONTH_NAME[i - 1], :num_books => 0, :index => i}
     }
     
     @last_year = @books.inject(distribution) {|dist, book|
-      dist[book.created_at.month - 1][:num_books] += 1
+      created_at = book.created_at
+      
+      unless created_at.month == this_month && created_at.year < this_year
+        dist[created_at.month - 1][:num_books] += 1
+      end
+      
       dist
-    }.partition {|m| m[:index] >= first_month }.flatten
+    }.partition {|m| m[:index] > this_month }.flatten
   end
   
 end
